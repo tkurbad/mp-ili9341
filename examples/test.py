05c3c7eb5b5e09c70378d98cf1646e8791467dc8
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2019-02-20 19:33:31 +0100

    Adapt to hwspi

--- examples/test.py
+++ examples/test.py
@@ -17,7 +17,15 @@ text = 'Now is the time for all good men to come to the aid of the party.'
 display = ILI9341(busid = VSPI, cs = 22, dc = 21, baudrate = 60000000)
 
 display.erase()
-display.set_pos(0, 0)
-for ff in fonts:
-    display.set_font(ff)
-    display.print(text)
+#display.set_pos(0, 0)
+#for ff in fonts:
+#    display.set_font(ff)
+#    display.print(text)
+
+display.fill_rectangle(0, 300, 240, 320, color = color565(200, 200, 200))
+display.set_color(color565(0, 0, 0), color565(200, 200, 200))
+display.set_font(tt14)
+x_extra = display._font.get_width(' ')
+x_tc1 = display.chars('TC1', 10, 302) + x_extra
+x_tc2 = display.chars('TC2', 90, 302) + x_extra
+x_tc3 = display.chars('TC3', 170, 302) + x_extra
